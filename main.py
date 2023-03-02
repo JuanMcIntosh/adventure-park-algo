31e7c8b3cbfdf5dd6def836096731a9a4887311b
juanmcintosh <thejuanmcintosh@gmail.com>
2023-03-01 21:54:14 -0500

    status function updates

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ premium_band_numbers = range(200, 249)
 
 BAND_TYPES = ["basic", "moderate", "premium"]
 
-# ride_status = 0
+
 band_number = int()
 band_type = str("")
 band_status = ""
@@ -17,36 +17,16 @@ band_status = ""
 
 rides = {"ATV": int(0), "Carting": int(0), "Paintballing": int(0)}
 
-# check band status funtion
-
-
-def status():
-    if band_type == str(BAND_TYPES[0]):
-        while rides.values("ATV") < B_MAX_RIDES:
-            # rides.update({"ATV":  })
-            # rides.values("ATV") =
-            rides.update({"ATV": rides.values("ATV" + int(1))})
-            print("Enjoy your " + rides.values(("ATV")) + "ride!")
-    else:
-        print("oyyee!!")
-    # elif band_type == BAND_TYPES[1]:
-
-
-# def status():
-#     for i in RIDES:
-#         if ride_status < B_MAX_RIDES:
-#             print("Go ahead! Enjoy your adventure!")
-#         else:
-#             print(
-#                 "You have used up all your rides on the tier ticket you purhased. You may purchase more rides if you wish."
-#             )
-#         # exit()
-
-
 # scan to detect armband within a certain proximity of scanner. Determine the class armband and the status.
 
 
 def scan():
+    global band_number
+    band_number = 0
+    global band_status
+    band_status = "True"
+    global band_type
+    band_type = ""
     while True:
         band_number = input("What is your wristband number?\n")
         if (
@@ -58,15 +38,18 @@ def scan():
             ):  # if the value stored in band_number is in basic_band_numbers (a range of numbers) then assign to band_type the string stored in the constant BAND_TYPES
                 band_type = str(BAND_TYPES[0])
                 print(band_type)
+                status()
                 exit()
             elif band_number in moderate_band_numbers:
                 band_type = str(BAND_TYPES[1])
                 print(band_type)
+                status()
                 exit()
             else:
                 if band_number in premium_band_numbers:
                     band_type = str(BAND_TYPES[-1])
                     print(band_type)
+                    status()
                     exit()
         else:
             print("This is not a number. Please enter the number on you wristband.\n")
@@ -77,11 +60,26 @@ def scan():
 # if not valid tell the person that have to purchase more rides
 
 
+def status():
+    global band_number
+    global band_status
+    global band_type
+    if band_type == "basic":  # BAND_TYPES[0]:
+        print("Band match!")
+        # if rides.values("ATV") < B_MAX_RIDES:
+        #     # rides.update({"ATV":  })
+        #     # rides.values("ATV") =
+        #     rides.update({"ATV": rides.values("ATV" + int(1))})
+        #     print("Enjoy your " + rides.values(("ATV")) + "ride!")
+        #     exit()
+    else:
+        print("No se.")
+    #     exit()
+    # elif band_type == BAND_TYPES[1]:
+
+
 def main():
     scan()
 
 
-status()
 main()
-
-kjnm
